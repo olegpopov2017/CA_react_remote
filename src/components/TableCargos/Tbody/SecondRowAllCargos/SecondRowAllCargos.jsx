a75d25274b4813f8340ef4edcdd6c581c428632d
oleg <olegpopov2017@mail.ru>
2025-04-03 18:51:59 +0300

    sucseess add editinible cell Xposition

--- src/components/TableCargos/Tbody/SecondRowAllCargos/SecondRowAllCargos.jsx
+++ src/components/TableCargos/Tbody/SecondRowAllCargos/SecondRowAllCargos.jsx
@@ -1,8 +1,8 @@
 import './SecondRowAllCargos.css'
-import UUID from './UUID/UUID'
 
+import UUID from './UUID/UUID'
 import Xwidth from './Xwidth/Xwidth'
-import Yheight from './Yheight/Yheigh'
+import Yheight from './Yheight/Yheight'
 import Zdepth from './Zdepth/Zdepth'
 import Xposition from './Xposition/Xposition'
 import Yposition from './Yposition/Yposition'
@@ -17,20 +17,38 @@ function SecondRowAllCargos({ cargos, setCargos }) {
     <tr key={item.uuid}>
       <UUID uuid={item.uuid} cargos={cargos} setCargos={setCargos} />
 
-      <Xwidth Xwidth={item.Xwidth} cargos={cargos} setCargos={setCargos} />
-      <Yheight Yheight={item.Yheight} cargos={cargos} setCargos={setCargos} />
-      <Zdepth Zdepth={item.Zdepth} cargos={cargos} setCargos={setCargos} />
+      <Xwidth
+        uuid={item.uuid}
+        Xwidth={item.Xwidth}
+        cargos={cargos}
+        setCargos={setCargos}
+      />
+      <Yheight
+        uuid={item.uuid}
+        Yheight={item.Yheight}
+        cargos={cargos}
+        setCargos={setCargos}
+      />
+      <Zdepth
+        uuid={item.uuid}
+        Zdepth={item.Zdepth}
+        cargos={cargos}
+        setCargos={setCargos}
+      />
       <Xposition
+        uuid={item.uuid}
         Xposition={item.Xposition}
         cargos={cargos}
         setCargos={setCargos}
       />
       <Yposition
+        uuid={item.uuid}
         Yposition={item.Yposition}
         cargos={cargos}
         setCargos={setCargos}
       />
       <Zposition
+        uuid={item.uuid}
         Zposition={item.Zposition}
         cargos={cargos}
         setCargos={setCargos}
--- src/components/TableCargos/Tbody/SecondRowAllCargos/Xposition/Xposition.jsx
+++ src/components/TableCargos/Tbody/SecondRowAllCargos/Xposition/Xposition.jsx
@@ -1,4 +1,49 @@
-function Xposition({ Xposition, cargos, setCargos }) {
-  return <td>{Xposition}</td>
+import React, { useState, useRef } from 'react'
+
+function Xposition({ uuid, Xposition: initialXposition, cargos, setCargos }) {
+  const [editing, setEditing] = useState({ id: null, field: null })
+  const inputRef = useRef()
+
+  const startEdit = (id, field) => {
+    setEditing({ id, field })
+    setTimeout(() => inputRef.current?.focus(), 0)
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      const newValue = e.target.value
+      if (newValue < 0) {
+        alert('введенные размеры не могут быть меньше 0')
+        return
+      }
+      setCargos((prevCargos) =>
+        prevCargos.map((cargo) =>
+          cargo.uuid === uuid
+            ? { ...cargo, Xposition: Number(newValue) }
+            : cargo
+        )
+      )
+      setEditing({ id: null })
+      console.log(cargos)
+    } else if (e.key === 'Escape') {
+      setEditing({ id: null })
+    }
+  }
+
+  return (
+    <td onClick={() => startEdit(uuid, 'Xpositionn')}>
+      {editing.id === uuid ? (
+        <input
+          ref={inputRef}
+          defaultValue={initialXposition}
+          onKeyDown={handleKeyDown}
+          onBlur={() => setEditing({ id: null })}
+        />
+      ) : (
+        initialXposition
+      )}
+    </td>
+  )
 }
+
 export default Xposition
