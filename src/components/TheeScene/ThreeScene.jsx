a935092f2df4d9ae8bc1395c2eff206ec6142c32
oleg <olegpopov2017@mail.ru>
2025-04-15 21:59:31 +0300

    adding orbit control

--- src/components/TheeScene/ThreeScene.jsx
+++ src/components/TheeScene/ThreeScene.jsx
@@ -1,6 +1,7 @@
 import './ThreeScene.css'
 import React, { useEffect, useRef } from 'react'
 import * as THREE from 'three'
+import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls'
 
 const ThreeScene = () => {
   const mountRef = useRef(null) // Ссылка на контейнер для сцены
@@ -8,15 +9,34 @@ const ThreeScene = () => {
   useEffect(() => {
     // Инициализация сцены, камеры и рендерера
     const scene = new THREE.Scene()
+    scene.background = new THREE.Color(0xf0ecf3)
     const camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     )
-
+    const light = new THREE.AmbientLight(0xffffbb, 0x080820, 1)
+    light.position.set(30, 30, 30).normalize()
+    scene.add(light)
     const renderer = new THREE.WebGLRenderer()
     renderer.setSize(600, 300) // Устанавливаем размеры канваса
+
+    //Adding groups for objects and another for pallets.
+    let cargo_area_group = new THREE.Group()
+    let cargo_group = new THREE.Group()
+    let group_of_cargo_area_floor = new THREE.Group() //Need for drag cargos and check collisions.
+    let group_of_cargo_area_attribute = new THREE.Group() //Contain rgb helpers,pictures and others visual atributes.
+
+    scene.add(cargo_area_group)
+    scene.add(cargo_group)
+    scene.add(group_of_cargo_area_floor)
+    scene.add(group_of_cargo_area_attribute)
+
+    //Rotation camera with orbit controls.
+    let controls = new OrbitControls(camera, renderer.domElement)
+    controls.enableDamping = true
+    controls.zoomSpeed = 6
 
     // Добавляем canvas только один раз
     if (!mountRef.current.hasChildNodes()) {
@@ -35,9 +55,6 @@ const ThreeScene = () => {
     // Функция анимации
     const animate = () => {
       requestAnimationFrame(animate) // Запрашиваем следующий кадр
-
-      cube.rotation.x += 0.01 // Вращение куба по оси X
-      cube.rotation.y += 0.01 // Вращение куба по оси Y
 
       renderer.render(scene, camera) // Рендерим сцену
     }
---
+++ src/custom objects/Cuboid.js
@@ -0,0 +1,14 @@
+export class Cuboid {
+  constructor(width_X, height_Y, depth_Z, position_x, position_y, position_z) {
+    this.width_X = width_X
+    this.height_Y = height_Y
+    this.depth_Z = depth_Z
+    this.position_x = position_x
+    this.position_y = position_y
+    this.position_z = position_z
+  }
+  uuid
+  CArea_id
+  is_arranged
+  color
+}
