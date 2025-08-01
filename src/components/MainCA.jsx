e690167ab0371d08f99f45e22f91e39304c4b486
oleg <olegpopov2017@mail.ru>
2025-08-01 21:41:10 +0300

    begin clean up code and rename folders

--- src/components/MainCA.jsx
+++ src/components/MainCA.jsx
@@ -3,6 +3,15 @@ import { useState } from 'react'
 import TableCargos from './dataTables/TableCargos/TableCargos.jsx'
 import TableCargoArea from './dataTables/TableCargoArea/TableCargoArea.jsx'
 import ThreeScene from './TheeJSComponent/ThreeScene.jsx'
+/**
+ * Main component that orchestrates the 3D scene and the data tables.
+ *
+ * This component manages the state for cargos and cargo areas, and
+ * passes down these states to the ThreeScene component for rendering
+ * the 3D environment and the TableCargos and TableCargoArea components
+ * for displaying and managing the cargo data.
+ */
+
 //
 function MainCA() {
   const [cargos, setCargos] = useState([])
