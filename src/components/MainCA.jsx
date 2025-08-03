4d9ba8be5e380b1473cac02722410b68e118dbc2
oleg <olegpopov2017@mail.ru>
2025-08-03 11:02:03 +0300

    allign tables

--- src/components/MainCA.jsx
+++ src/components/MainCA.jsx
@@ -1,33 +1,34 @@
 import { useState } from 'react'
 
 import TableCargos from './dataTables/TableCargos/TableCargos.jsx'
-import TableCargoArea from './dataTables/TableCargoArea/CargoAreaForm.jsx'
-import ThreeScene from './TheeJSComponent/ThreeScene.jsx'
 import CargoAreaForm from './dataTables/TableCargoArea/CargoAreaForm.jsx'
-/**
- * Main component that orchestrates the 3D scene and the data tables.
- *
- * This component manages the state for cargos and cargo areas, and
- * passes down these states to the ThreeScene component for rendering
- * the 3D environment and the TableCargos and TableCargoArea components
- * for displaying and managing the cargo data
- */
+import ThreeScene from './TheeJSComponent/ThreeScene.jsx'
 
-//
+import './MainCA.css'
+
 function MainCA() {
   const [cargos, setCargos] = useState([])
   const [cargoArea, setCargoArea] = useState('')
+
   return (
-    <>
-      <ThreeScene
-        cargos={cargos}
-        setCargos={setCargos}
-        cargoArea={cargoArea}
-        setCargoArea={setCargoArea}
-      />
-      <TableCargos cargos={cargos} setCargos={setCargos} />
-      <CargoAreaForm cargoArea={cargoArea} setCargoArea={setCargoArea} />
-    </>
+    <div className="main-container">
+      <div className="scene-wrapper">
+        <ThreeScene
+          cargos={cargos}
+          setCargos={setCargos}
+          cargoArea={cargoArea}
+          setCargoArea={setCargoArea}
+        />
+      </div>
+      <div className="tables-row">
+        <div className="table-left">
+          <CargoAreaForm cargoArea={cargoArea} setCargoArea={setCargoArea} />
+        </div>
+        <div className="table-right">
+          <TableCargos cargos={cargos} setCargos={setCargos} />
+        </div>
+      </div>
+    </div>
   )
 }
 
