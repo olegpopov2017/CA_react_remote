89df11c829b98bdd0b33bd21413b3f1c3e0964d0
oleg <olegpopov2017@mail.ru>
2025-08-08 21:28:02 +0300

    Continue creating modal.

--- src/components/modals/SendToAlgorithmModal/SendToAlgorithmModal.jsx
+++ src/components/modals/SendToAlgorithmModal/SendToAlgorithmModal.jsx
@@ -1,18 +1,78 @@
 import './SendToAlgorithmModal.css'
 
-const SendToAlgorithmModal = ({
+import { useState } from 'react'
+
+function SendToAlgorithmModal({
   cargos,
   setCargos,
   cargoArea,
   setCargoArea,
   onClose,
-}) => {
+}) {
+  const [selectedCorner, setSelectedCorner] = useState('')
+
+  const handleChange = (event) => {
+    setSelectedCorner(event.target.value)
+  }
   return (
     <div className="modal-overlay">
       <div className="modal">
-        <h2>THIS IS MODAL</h2>
-        <p>Тут можно что-то сделать с данными</p>
-        <button onClick={onClose}>Закрыть</button>
+        <div>
+          {/* Выберите параметры расстановки: */}
+          <div>
+            Угол относительно которого будет расстановка:
+            <label>
+              <input
+                type="radio"
+                name="corner"
+                value="1"
+                onChange={handleChange}
+              />
+              передний левый
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="corner"
+                value="2"
+                onChange={handleChange}
+              />
+              передний правый
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="corner"
+                value="3"
+                onChange={handleChange}
+              />
+              задний левый
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="corner"
+                value="4"
+                onChange={handleChange}
+              />
+              задний правый
+            </label>
+          </div>
+          <div>
+            Требуется ли расстановка в линию
+            <label>
+              <input type="checkbox" />
+            </label>
+          </div>
+          <div>
+            Требуется ли расстановка от центра грузового помещения
+            <label>
+              <input type="checkbox" />
+            </label>
+          </div>
+          <button onClick={onClose}>Рассчитать</button>
+          <button onClick={onClose}>Закрыть</button>
+        </div>
       </div>
     </div>
   )
