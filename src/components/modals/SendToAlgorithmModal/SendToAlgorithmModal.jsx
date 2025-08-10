f7dce14bec65a49966a5cd25cb71667bfa535337
oleg <olegpopov2017@mail.ru>
2025-08-10 10:07:06 +0300

    Continue create modal.

--- src/components/modals/SendToAlgorithmModal/SendToAlgorithmModal.jsx
+++ src/components/modals/SendToAlgorithmModal/SendToAlgorithmModal.jsx
@@ -9,70 +9,81 @@ function SendToAlgorithmModal({
   setCargoArea,
   onClose,
 }) {
-  const [selectedCorner, setSelectedCorner] = useState('')
+  const [selectedCorner, setSelectedCorner] = useState('1')
+  const [isLineArrangement, setIsLineArrangement] = useState(true)
+  const [isCenterArrangement, setIsCenterArrangement] = useState(false)
 
-  const handleChange = (event) => {
+  const changeCornerOfArrangement = (event) => {
     setSelectedCorner(event.target.value)
   }
   return (
-    <div className="modal-overlay">
+    <div className="container-modal" role="dialog" aria-modal="true">
       <div className="modal">
+        <h2>Выберите параметры расстановки</h2>
+        <fieldset>
+          <legend>Угол относительно которого будет расстановка:</legend>
+          <label className="change-corner">
+            <input
+              className="radio"
+              type="radio"
+              name="corner"
+              value="1"
+              defaultChecked
+              onChange={changeCornerOfArrangement}
+            />
+            передний левый
+          </label>
+          <label className="change-corner">
+            <input
+              className="radio"
+              type="radio"
+              name="corner"
+              value="2"
+              onChange={changeCornerOfArrangement}
+            />
+            передний правый
+          </label>
+          <label className="change-corner">
+            <input
+              className="radio"
+              type="radio"
+              name="corner"
+              value="3"
+              onChange={changeCornerOfArrangement}
+            />
+            задний левый
+          </label>
+          <label className="change-corner">
+            <input
+              className="radio"
+              type="radio"
+              name="corner"
+              value="4"
+              onChange={changeCornerOfArrangement}
+            />
+            задний правый
+          </label>
+        </fieldset>
         <div>
-          {/* Выберите параметры расстановки: */}
-          <div>
-            Угол относительно которого будет расстановка:
-            <label>
-              <input
-                type="radio"
-                name="corner"
-                value="1"
-                onChange={handleChange}
-              />
-              передний левый
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="corner"
-                value="2"
-                onChange={handleChange}
-              />
-              передний правый
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="corner"
-                value="3"
-                onChange={handleChange}
-              />
-              задний левый
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="corner"
-                value="4"
-                onChange={handleChange}
-              />
-              задний правый
-            </label>
-          </div>
-          <div>
-            Требуется ли расстановка в линию
-            <label>
-              <input type="checkbox" />
-            </label>
-          </div>
-          <div>
-            Требуется ли расстановка от центра грузового помещения
-            <label>
-              <input type="checkbox" />
-            </label>
-          </div>
-          <button onClick={onClose}>Рассчитать</button>
-          <button onClick={onClose}>Закрыть</button>
+          Требуется ли расстановка в линию
+          <input
+            className="checkbox-line-arrangement"
+            type="checkbox"
+            checked={isLineArrangement}
+            onChange={() => setIsLineArrangement(!isLineArrangement)}
+          />
         </div>
+        <div>
+          Требуется ли расстановка от центра грузового помещения
+          <input
+            className="checkbox-center-arrangement"
+            type="checkbox"
+            checked={isCenterArrangement}
+            onChange={() => setIsCenterArrangement(!isCenterArrangement)}
+          />
+        </div>
+        <button onClick={onClose}>Рассчитать</button>
+        <button onClick={onClose}>Закрыть</button>
       </div>
     </div>
   )
