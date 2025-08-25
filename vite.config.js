8e2eebbc557c37537be9362547a77b18a31a5b3d
oleg <olegpopov2017@mail.ru>
2025-08-25 15:26:30 +0300

    Continue join server and client.

--- vite.config.js
+++ vite.config.js
@@ -13,14 +13,14 @@ export default defineConfig({
 
   // For disable CORS
 
-  //server: {
-  //  proxy: {
-  //    "/api": {
-  //      target: "http://127.0.0.1:3000",
-  //      changeOrigin: true,
-  //      rewrite: (path) => path.replace(/^\/api/, ""),
-  //      // => /api -> /
-  //    },
-  //  },
-  //},
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        // => /api -> /
+      },
+    },
+  },
 })
