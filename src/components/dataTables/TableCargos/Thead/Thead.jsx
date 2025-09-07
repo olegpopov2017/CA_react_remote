9d7bbe2b23dcb9ee4822e3ed324d571ab5d0bee3
oleg <olegpopov2017@mail.ru>
2025-09-07 09:22:01 +0300

    test commit.

--- src/components/dataTables/TableCargos/Thead/Thead.jsx
+++ src/components/dataTables/TableCargos/Thead/Thead.jsx
@@ -15,11 +15,11 @@ function Thead({ cargos, setCargos }) {
         <th>X position</th>
         <th>Y position</th>
         <th>Z position</th>
-        {/* <th>Weight</th>
+        <th>Weight</th>
         <th>Name</th>
         <th>ID</th>
         <th>Priority</th>
-        <th>Layar</th> */}
+        <th>Layar</th>
 
         <th className="finishTH">
           <button onClick={() => deleteAllCargos()}>Удалить все</button>
