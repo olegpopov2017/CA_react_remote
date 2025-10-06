5fe41372fb449d956797d8a046daf4499fe69cae
Oleg <olegpopov2017@mail.ru>
2025-10-06 11:37:45 +0300

    Test commit.

--- vite.config1.js
+++ vite.config1.js
@@ -4,11 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // ЗАМЕНИТЕ на имя вашего репозитория
   build: {
     outDir: 'docs', // Папка для сборки
-    assetsDir: 'assets',
-    emptyOutDir: true,
     sourcemap: false, // Карты кода для отладки (можно отключить в продакшене)
     minify: true, // ← тут было 'true' строкой, лучше булевый тип
     chunkSizeWarningLimit: 1000, // Лимит для предупреждений о размере чанков
